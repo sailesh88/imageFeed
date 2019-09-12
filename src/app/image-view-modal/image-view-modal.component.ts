582b4b9ff4c3f3b830063facde838e07b4ef8519
sailesh guedul <guedulsailesh@gmail.com>
2019-09-12 20:11:53 +0530

    Added Few functions and rectified all the errors

--- src/app/image-view-modal/image-view-modal.component.ts
+++ src/app/image-view-modal/image-view-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-image-view-modal',
@@ -7,17 +8,37 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./image-view-modal.component.css']
 })
 export class ImageViewModalComponent implements OnInit {
+  changeText: boolean;
 
   constructor(
     public dialogRef: MatDialogRef<ImageViewModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ImageData
-  ) { }
+  ) {
+    this.changeText = false;
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   ngOnInit() {
+  }
+
+
+  click(x) {
+    //console.log(x);
+  }
+
+  downloadFile1() {
+    var htmlToImage = require('html-to-image');
+
+    htmlToImage.toJpeg(document.getElementById('base'), { quality: 0.95 })
+      .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'my-image-name.jpeg';
+        link.href = dataUrl;
+        link.click();
+      });
   }
 
 }
